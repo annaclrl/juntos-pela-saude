d0d28188bf3fe2716d0b835ba7d12bacc19211df
Anna <annaclararussoluca@gmail.com>
2025-08-17 18:29:57 -0300

    Criando e estilizando componente Rodape

---
+++ src/components/Rodape/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer>
+            <Link to={"./pages/integrantes"}>Integrantes</Link>
+            <p>© 2025 Juntos pela Saúde. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
+
+export default Rodape;
+  
