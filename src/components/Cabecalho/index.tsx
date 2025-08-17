a8099d864838d52c1d7ca1606aac168edfe923d1
Anna <annaclararussoluca@gmail.com>
2025-08-17 18:29:37 -0300

    Criando e estilizando componente Cabecalho

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,29 @@
+import './Cabecalho.css'
+import LogoBranca from '../../assets/images/logo-completa-branca.png'
+import IconUsuario from '../../assets/icons/icone-usuario.png'
+import { Link } from 'react-router-dom'
+
+const Cabecalho = () => {
+    return(
+        <header>
+            <img className="imagem-logo" src={LogoBranca} alt="Logo branca da Juntos pela Saúde" />
+
+        <nav>
+            <ul>
+                <li><Link to={"/"} className="active">Início</Link></li>
+                <li><Link to={"/agenda"}>Agenda</Link></li>
+                <li><Link to={"/faq"}>FAQ</Link></li>
+                <li><Link to={"/sobre"}>Sobre</Link></li>
+            </ul>
+        </nav>
+
+         <button id="btn_user">
+             <img className="icone-usuario" src={IconUsuario} alt="Icone do usuário"/>
+        </button>
+
+        </header>
+        
+    )
+}
+
+export default Cabecalho;
