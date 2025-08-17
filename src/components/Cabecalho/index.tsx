0f861bf20b5108c06725ccf2004f8adabe0346f5
Anna <annaclararussoluca@gmail.com>
2025-08-17 19:27:36 -0300

    Melhorando nome das classe e id

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -4,25 +4,24 @@ import IconUsuario from '../../assets/icons/icon-usuario.png'
 import { Link } from 'react-router-dom'
 
 const Cabecalho = () => {
-    return(
+    return (
         <header>
             <img className="imagem-logo" src={LogoBranca} alt="Logo branca da Juntos pela Saúde" />
 
-        <nav>
-            <ul>
-                <li><Link to={"/"} className="active">Início</Link></li>
-                <li><Link to={"/agenda"}>Agenda</Link></li>
-                <li><Link to={"/faq"}>FAQ</Link></li>
-                <li><Link to={"/sobre"}>Sobre</Link></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    <li><Link to={"/"} className="active">Início</Link></li>
+                    <li><Link to={"/agenda"}>Agenda</Link></li>
+                    <li><Link to={"/faq"}>FAQ</Link></li>
+                    <li><Link to={"/sobre"}>Sobre</Link></li>
+                </ul>
+            </nav>
 
-         <button id="btn_user">
-             <img className="icone-usuario" src={IconUsuario} alt="Icone do usuário"/>
-        </button>
+            <button id="btn_user">
+                <img className="icone-usuario" src={IconUsuario} alt="Icone do usuário" />
+            </button>
 
         </header>
-        
     )
 }
 
--- src/pages/PaginaInicial/index.tsx
+++ src/pages/PaginaInicial/index.tsx
@@ -8,33 +8,33 @@ import { Link } from 'react-router-dom'
 
 const PaginaInicial = () =>{
     return(
-        <main>
-        <section id="secao1">
-            <div className="secao1_texto">
+        <main id="container_pagina_inicial">
+        <section id="container_pagina_inicial_secao_apresentacao">
+            <div className="container_pagina_inicial_secao_apresentacao_texto">
                 <h1>Boas-Vindas!</h1>
                 <p>Sua <span>saúde</span> mais próxima de você.</p>
                 <p>Aqui, cuidar da saúde é <span>simples</span> e <span>digital</span>.</p>
             </div>
-            <div className="secao1_imagem">
+            <div className="container_pagina_inicial_secao_apresentacao_imagem">
                 <img className="imagem-pagina-principal" src={ImagemPrincipal} alt="Representação de uma consulta online" />
             </div>
         </section>
-        <section id="secao2">
+        <section id="container_pagina_inicial_secao_funcionalidades">
             <h2>Como funciona?</h2>
 
-            <div className="secao2_icones">
+            <div className="container_pagina_inicial_secao_funcionalidades_cards">
                 
-                <div className="funcionalidades_pagina_inicial">
+                <div>
                     <img src={IconCalendario} alt="Icone de calendário" />
                     <Link to={"/agendar-consulta"}>Agende sua consulta</Link>
                 </div>
 
-                <div className="funcionalidades_pagina_inicial">
+                <div>
                     <img src={IconDuvida} alt="Icone de dúvida" />
                     <Link to={"/faq"}>Tire suas dúvidas</Link>
                 </div>
 
-                <div className="funcionalidades_pagina_inicial">
+                <div>
                     <img src={IconChat} alt="Icone de chat" />
                     <Link to={"/contato"}>Dê sua opinião</Link>
                 </div>
